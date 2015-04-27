f269a5ef470157835f1029bb9db6c2422ab78a5e
vitrum <vitrum.cn@gmail.com>
2015-04-27 15:35:51 +0800

    添加了声音，代码是从微信活动里来的，测试BGM也是。

--- www/js/about.js
+++ www/js/about.js
@@ -91,6 +91,24 @@ $(document).ready(function() {
         swiperAnimate(swiper);
     });
 
+    function r() {
+        t.show(), e.play(), t.removeClass("music-open").addClass("music-close")
+    }
+
+    function i() {
+        e.pause(), t.removeClass("music-close").addClass("music-open")
+    }
+    var e = $("#audio")[0],
+        t = $(".music"),
+        n = this;
+    t.on("click", function(e) {
+        var t = $(this);
+        t.is(".music-open") ? r() : i(), e.preventDefault()
+    }), window.onblur = function() {
+        i()
+    }, this.play = r, this.stop = i
+
+
 });
 
 
